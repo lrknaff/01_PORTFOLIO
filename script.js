de5072d9a2280275cbe81376a55d42714fabd1cc
Lacey Knaff <lacey.knaff@gmail.com>
2016-09-04 14:04:39 -0600

    updated social icon size to be slightly larger and have more padding

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-var aboutButton = document.querySelector('#about');
-var projectsButton = document.querySelector('#projects');
-var contactButton = document.querySelector('#contact');
 
-
-function hideAboutSection() {
-  aboutSelection.style.visibility = "hidden";
-}
-
-aboutButton.addEventListener('click', function() {
-  var aboutSection = document.querySelector('#about-me');
-  hideAboutSection(aboutSelection);
-})
