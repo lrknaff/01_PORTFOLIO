696dd94fadcaf3dc9432793831a77158a6345a06
Lacey Knaff <lacey.knaff@gmail.com>
2016-09-05 16:33:47 -0600

    added toggle class to hamburger nav to hide and display navigation items and page sections. still need to work on centering navigation items

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+$('#nav-inner').hide();
+
+function showHideNav () {
+  if ($('#hamburger-nav').hasClass('open')) {
+    $('#nav-inner').show();
+    $('#about-me').hide();
+    $('.experience').hide();
+    $('.education').hide();
+    $('.resume-button').hide();
+  } else {
+    $('#nav-inner').hide();
+    $('#about-me').show();
+    $('.experience').show();
+    $('.education').show();
+    $('.resume-button').show();
+  }
+};
+
+
+
 $(document).ready(function() {
-  $('#nav-icon3').click(function(){
+  $('#hamburger-nav').click(function(){
 		$(this).toggleClass('open');
+    showHideNav ();
 	});
 });
