f8e732f9758ff8857aaeac15cc894a7f843df738
Lacey Knaff <lacey.knaff@gmail.com>
2016-09-06 16:08:04 -0600

    added event listener to about navigation link to go back to about me page

--- script.js
+++ script.js
@@ -16,11 +16,25 @@ function showHideNav () {
   }
 };
 
+function showAboutPage() {
+  $('#nav-inner').hide();
+  $('#about-me').show();
+  $('.experience').hide();
+  $('.education').hide();
+  $('.resume-button').hide();
+}
 
+function toggleHamburgerNav() {
+  $('#hamburger-nav').toggleClass('open');
+}
 
 $(document).ready(function() {
-  $('#hamburger-nav').click(function(){
-		$(this).toggleClass('open');
+  $('#hamburger-nav').click(function() {
+		toggleHamburgerNav ();
     showHideNav ();
 	});
+  $('#about-link').click(function() {
+    showAboutPage ();
+    toggleHamburgerNav ();
+  });
 });
